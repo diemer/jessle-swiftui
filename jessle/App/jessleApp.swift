275eced9299b5db1b81991b33df25ce9719685bb
Dan Diemer <dan@headway.io>
2022-02-11 21:40:04 -0600

    Create app layer

--- jessle/App/jessleApp.swift
+++ jessle/App/jessleApp.swift
@@ -13,7 +13,7 @@ struct jessleApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
