73e1a857b0ac0b73b89044543b61866dc23bdfb7
Dan Diemer <dan@headway.io>
2022-02-11 22:54:23 -0600

    Add letter guess enum

---
+++ jessle/Presentation/Enums/LetterGuess.swift
@@ -0,0 +1,15 @@
+//
+//  LetterGuess.swift
+//  jessle
+//
+//  Created by Dan Diemer on 2/11/22.
+//
+
+import Foundation
+
+enum LetterGuess {
+  case unguessed
+  case wrong
+  case inWord
+  case correct
+}
