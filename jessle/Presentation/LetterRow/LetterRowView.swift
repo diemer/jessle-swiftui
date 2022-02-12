a1278dc74b4248e5fbc48e657292b572577e9d57
Dan Diemer <dan@headway.io>
2022-02-12 00:02:40 -0600

    Add letter row view

--- jessle/Presentation/LetterRow/LetterRowView.swift
+++ jessle/Presentation/LetterRow/LetterRowView.swift
@@ -7,14 +7,51 @@
 
 import SwiftUI
 
-struct LetterRowVIew: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct LetterRowView: View {
+ 
+  var viewModel: LetterRowViewModel = LetterRowViewModel()
+  
+  var body: some View {
+    LazyHGrid(rows: viewModel.rows, alignment: .center){
+      let squaresData = Array(viewModel.squaresData.enumerated())
+      ForEach(squaresData, id: \.0) { item in
+        let squareContent = item.element.squareContent
+        let squareType = item.element.squareType
+        LetterSquareView(squareType: squareType, squareContent: squareContent)
+          .aspectRatio(1, contentMode: .fit)
+//        Text("\(item)")
+      }
     }
+    .padding(.horizontal)
+    
+  }
 }
 
-struct LetterRowVIew_Previews: PreviewProvider {
-    static var previews: some View {
-        LetterRowVIew()
-    }
+typealias SquareData = (squareType: LetterGuess, squareContent: String?)
+typealias SquaresData = [SquareData]
+typealias RowData = [SquaresData]
+extension LetterRowView{
+  
+  init(squaresData: SquaresData, squareSize: CGFloat) {
+    let viewModel = LetterRowViewModel(squaresData: squaresData, squareSize: squareSize)
+    self.init(viewModel: viewModel)
+  }
+  
+}
+
+struct LetterRowView_Previews: PreviewProvider {
+  static var rowData: SquaresData {
+    return [
+      (squareType: LetterGuess.correct, squareContent: "W"),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil)
+    ]
+  }
+  static var previews: some View {
+    LetterRowView(squaresData: rowData, squareSize: 50)
+      .frame(minWidth: 400)
+  }
 }
