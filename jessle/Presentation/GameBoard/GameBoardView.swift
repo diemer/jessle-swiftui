e8e99c0a297c7cf58a819843ab21cc8869195e08
Dan Diemer <dan@headway.io>
2022-02-12 00:24:15 -0600

    Add initial stab at game board

--- jessle/Presentation/GameBoard/GameBoardView.swift
+++ jessle/Presentation/GameBoard/GameBoardView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct GameBoardView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var viewModel: GameBoardViewModel = GameBoardViewModel()
+  
+  var body: some View {
+    let rowData = Array(viewModel.rowData.enumerated())
+    GeometryReader { g in
+      let geoW = g.size.width
+      let squareSize = geoW / Constants.Game.NumberOfSquares - (Constants.Shape.DefaultSquareSpacing * Constants.Game.NumberOfSquares - 1)
+      ForEach(rowData, id: \.0) { row in
+        LetterRowView(squaresData: row.element, squareSize: squareSize)
+      }
     }
+  }
 }
 
 struct GameBoardView_Previews: PreviewProvider {
-    static var previews: some View {
-        GameBoardView()
-    }
+  static var gameViewModel: GameBoardViewModel {
+    let firstRow = [
+      (squareType: LetterGuess.correct, squareContent: "W"),
+      (squareType: LetterGuess.wrong, squareContent: "A"),
+      (squareType: LetterGuess.wrong, squareContent: "S"),
+      (squareType: LetterGuess.wrong, squareContent: "P"),
+      (squareType: LetterGuess.wrong, squareContent: "S")
+    ]
+    let secondRow = [
+      (squareType: LetterGuess.correct, squareContent: "W"),
+      (squareType: LetterGuess.inWord, squareContent: "I"),
+      (squareType: LetterGuess.unguessed, squareContent: "L"),
+      (squareType: LetterGuess.unguessed, squareContent: "L"),
+      (squareType: LetterGuess.unguessed, squareContent: "S")
+    ]
+    let thirdRow = [
+      (squareType: LetterGuess.unguessed, squareContent: ""),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil),
+      (squareType: LetterGuess.unguessed, squareContent: nil)
+    ]
+
+    let vm = GameBoardViewModel()
+    vm.rowData = [
+      firstRow,
+      secondRow,
+      thirdRow
+    ]
+    return vm
+  }
+  static var previews: some View {
+    GameBoardView(viewModel: gameViewModel)
+  }
 }
--- jessle/Presentation/GameBoard/GameBoardViewModel.swift
+++ jessle/Presentation/GameBoard/GameBoardViewModel.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+class GameBoardViewModel: ObservableObject {
+  @Published var rowData: RowData = []
+}
