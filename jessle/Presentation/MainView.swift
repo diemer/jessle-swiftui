34ce422bc7d643217e00a880377a398b7ea8037e
Dan Diemer <dan@headway.io>
2022-02-11 21:40:53 -0600

    Create presentation layer

---
+++ jessle/Presentation/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  jessle
+//
+//  Created by Dan Diemer on 2/11/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
