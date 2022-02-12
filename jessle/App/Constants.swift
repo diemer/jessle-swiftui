0757b9caca838547c88c256dd915a3a49e5a885c
Dan Diemer <dan@headway.io>
2022-02-11 21:40:37 -0600

    Add color constants

---
+++ jessle/App/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  jessle
+//
+//  Created by Dan Diemer on 2/11/22.
+//
+
+import Foundation
+
+enum Constants {
+  enum Colors: String {
+    case yellow
+    case gray
+    case lightGray
+    case white
+  }
+}
