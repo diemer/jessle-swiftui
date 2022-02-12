94b28ec1f67f72a6d7d358c1428c7ea7f7c97f62
Dan Diemer <dan@headway.io>
2022-02-12 00:02:46 -0600

    Add letter row view model

--- jessle/Presentation/LetterRow/LetterRowViewModel.swift
+++ jessle/Presentation/LetterRow/LetterRowViewModel.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
 
 class LetterRowViewModel: ObservableObject {
+  var squareSize: CGFloat = Constants.Shape.DefaultSquareSize
+  var squareSpacing: CGFloat = Constants.Shape.DefaultSquareSpacing
+  var squaresData: SquaresData = []
+  var rows: [GridItem] {
+    return [
+      GridItem(.fixed(squareSize), spacing: squareSpacing, alignment: .center)
+    ]
+  }
   
+  convenience init(squaresData: SquaresData, squareSize: CGFloat) {
+    self.init()
+    self.squaresData = squaresData
+    self.squareSize = squareSize
+  }
+
 }
