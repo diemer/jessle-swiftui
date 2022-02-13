fe80347806a195abe853723b9473b9c6239073da
Dan Diemer <dan@headway.io>
2022-02-13 09:56:20 -0600

    Add KeyboardKeyView content

--- jessle/Presentation/Keyboard/KeyboardKeyView.swift
+++ jessle/Presentation/Keyboard/KeyboardKeyView.swift
@@ -8,10 +8,31 @@
 import SwiftUI
 
 struct KeyboardKeyView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var viewModel: KeyboardKeyViewModel = KeyboardKeyViewModel()
+  
+  var body: some View {
+    Button(action: viewModel.onPress) {
+      if viewModel.type == .Backspace {
+        Image(systemName: Constants.Icons.Backspace)
+      } else {
+        Text(viewModel.textContent.uppercased())
+          .bold()
+      }
     }
+    .disabled(viewModel.disabled)
+  }
+
 }
+  
+extension KeyboardKeyView {
+  
+  enum KeyType {
+    case Letter
+    case Enter
+    case Backspace
+  }
+}
+
 
 struct KeyboardKeyView_Previews: PreviewProvider {
     static var previews: some View {
