446f78dadae6a6c69528857350d6546024bcea32
Dan Diemer <dan@headway.io>
2022-02-13 09:01:02 -0600

    Add KeyboardKeyView and View Model

---
+++ jessle/Presentation/Keyboard/KeyboardKeyView.swift
@@ -0,0 +1,20 @@
+//
+//  KeyboardKeyView.swift
+//  jessle
+//
+//  Created by Dan Diemer on 2/13/22.
+//
+
+import SwiftUI
+
+struct KeyboardKeyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KeyboardKeyView_Previews: PreviewProvider {
+    static var previews: some View {
+        KeyboardKeyView()
+    }
+}
---
+++ jessle/Presentation/Keyboard/KeyboardKeyViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  KeyboardKeyViewModel.swift
+//  jessle
+//
+//  Created by Dan Diemer on 2/13/22.
+//
+
+import Foundation
